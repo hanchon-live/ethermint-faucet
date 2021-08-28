1257887a4e2a0c2723700cff4b77fab71b8821a2
milagros paoletti <milagros.paoletti@gmail.com>
2021-08-28 19:50:58 +0200

    update padding top header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import { NavbarBase } from "./NavbarBase";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+     {
+        padding-top: 0.7em;
+    }
+`;
 
 export const Header = () => {
     return (
-        <header>
+        <StyledHeader>
             <NavbarBase />
-        </header>
+        </StyledHeader>
     );
 };
